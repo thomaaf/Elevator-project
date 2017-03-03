3677d19b3647d68b51381203b70bc533132a0f54
thomaaf <95frekhaug@gmail.com>
2017-03-03 13:59:22 +0100

    added comments

--- eventmanager.c
+++ eventmanager.c
@@ -4,54 +4,64 @@
 #include <stdio.h>
 
 
-static int list[4][3]={ //[floor][button]
+static int list[4][3]={ //[floor][button]		//reminder: 1st floor in top of list, 4th floor at bottom, need to fix (the asset.c/prinlist prints opposite -> 1st at bottom)
 	{0,0,0},
 	{0,0,0},
 	{0,0,0},
-	{0,0,0}
+	{0,0,0} //
 };
 
-static int Currentfloor;
+static int Currentfloor;//private variable, holds first floor
 
 
 void emQueueUpdater( int pressedFloor, int pressedButton ){ //updates the queue with new orderd
-	list [pressedFloor][pressedButton]=1;
-	printlist(list);
-	emExecutePriorityOrder(pressedFloor,pressedButton);
+	list [pressedFloor][pressedButton]=1;					//sets new order, internal or external
+	printlist(list);										
+	emExecutePriorityOrder(pressedFloor,pressedButton);		//tells it that there is a order, and that something should be done
 }
 
-void emElevatorFeedBack(int newFloor){ //Updates the queue when/if and order has been executed
-	if (Currentfloor==newFloor){ //if there's no new floor update, break to avoid more work
-		//do nothing
+void emElevatorFeedBack(int newFloor){ 						//Updates the queue when/if and order has been executed 		//should also have a better name;
+	if (Currentfloor==newFloor){ 							//if there's no new floor update, break to avoid more work
+		//if the order is is equal to the current floor, 
+		//then the door should open, should this be 
+		//implemented here? if so, how often shoud
+		//this function run
 	}
 	else{
-		Currentfloor=newFloor;
+		Currentfloor=newFloor;						
 		for (int button=0; button <3; button++){
 			if (list[Currentfloor][button]==1 ){
-				list[Currentfloor][button]=0;
-				printlist(list);
+				emExecutePriorityOrder(newFloor); 			//stops the elevator, finishes the execution
+				list[Currentfloor][button]=0;				//clears the order, and it is marked as complete
+				EMDoorControl();							//Opens the door, whole program busy for 3 seconds
+				printlist(list);							//prints the new list with the order marked as cleared
 			}
 		}
 	}
 }
 
+
 void emExecutePriorityOrder(int pressedFloor,int pressedButton){
-	printf("pressedFloor %d\n",pressedFloor);	
-	printf("currentFloor %d\n",Currentfloor);	
+
 	if (pressedFloor>Currentfloor){
 		elev_set_motor_direction(DIRN_UP);
-		printf("test1");	
+	
 
 	}
 	else if(pressedFloor<Currentfloor){
 		elev_set_motor_direction(DIRN_DOWN);
-		printf("test2");
+	
 	}
 	else{
 		elev_set_motor_direction(DIRN_STOP);
-		printf("test3");
+		
 	}
 
 }
 
 
+void EMDoorControl(){
+	elev_set_door_open_lamp(1);	//opens the door
+	sleep(3);					//Timer at 3 seconds (bad implementation)
+	elev_set_door_open_lamp(0);	//closes the door
+}
--- eventmanager.h
+++ eventmanager.h
@@ -3,6 +3,7 @@
 void emQueueUpdater( int pressedFloor, int pressedButton );
 void emElevatorFeedBack(int newFloor);
 void emExecutePriorityOrder(int pressedFloor,int pressedButton);
+void EMDoorControl();
 //void emCheckForMatch(int floor);
 //void emmatchFound();
 
