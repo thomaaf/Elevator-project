c7ba4b69d23bbf3c5731274411bfa0317da02e6b
thomaaf <95frekhaug@gmail.com>
2017-03-17 11:53:48 +0100

    Update eventmanager.h

--- eventmanager.h
+++ eventmanager.h
@@ -1,21 +1,16 @@
 
-int intransition;
+int INTRANSITION;
 
 
-void emQueueUpdater( int pressed_floor, int pressed_button );
-
-void emFloorControl(int newFloor);
-void queuecheck();
-void emSetEngine(int direction);
-void emDoorControl(int floor);
-void emStopButton();
-void emToggleTimer(int active );
-void emCheckTimer();
-int  emcheckSpecialCase( );
-int  emcheckForMoreOrder();
-
-void emprintshit();
-
+void em_Queue_Updater			( int pressedFloor, int pressedButton );
+void em_Floor_Control			( int newFloor );
+void em_Queue_Check				( void );
+void em_Set_Engine				( int direction );
+void em_Door_Control			( int floor );
+void em_Stop_Button				( void );
+int  em_Check_Special_Case		( void );
+int  em_Check_For_More_Orders	( void );
+void em_Print_Status			( void );
 
 
 
